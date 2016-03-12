fce99a6babb435611634ed0d71296b39160ca6ba
Andrew Danylenko <wdanylenko@gmail.com>
2016-03-11 20:33:33 -0800

    Initial commit

---
+++ src/com/company/AreaOfTriangle.java
@@ -0,0 +1,29 @@
+package com.company;
+
+/**
+ * Created by User on 11.03.2016.
+ */
+public class AreaOfTriangle {
+    public AreaOfTriangle() {
+    }
+
+    public static void main(String[] args) {
+        byte b = 2;
+        byte h = 1;
+        byte r = 1;
+        boolean Radius = true;
+        boolean base = true;
+        boolean height = true;
+        boolean AreaOfTriangle = true;
+        boolean AreaOfRectangle = true;
+        boolean AreaOfCircle = true;
+        int c = b * h / 2;
+        int v = b * h;
+        double a = 2.0D;
+        double d = Math.pow((double)r, a);
+        double k = 3.14D * d;
+        System.out.println(c);
+        System.out.println(v);
+        System.out.println(k);
+    }
+}
